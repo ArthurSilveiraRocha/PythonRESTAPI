242492544d4166d5982aa5cd4660a4c23110a64f
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2023-05-09 14:54:42 -0300

    Create Utilidades.py

---
+++ Utilidades.py
@@ -0,0 +1,31 @@
+#Métodos
+
+def metodo(valor1,valor2):
+    return print(valor1 + valor2)
+metodo(4,4)
+
+#Built-In
+
+print('daoscwiueobn')
+
+print(int(12.4),"converte em inteiro")
+
+print(float(10),"converte em decimal")
+
+print(len("Abacateiro                     "),"conta os objetos")
+
+x = -10
+
+print(abs(x),"mostra o valor absoluto, independente do sinal")
+
+lista = [1,2,3,4,5,6,7,8,9]
+
+print(sum(lista),"soma todos os valores")
+
+print(min(lista),"mostra o valor minimo")
+
+print(max(lista),"mostra o valor maximo")
+
+print(round(23.123213, 2),"arredonda o valor")
+
+
