604b09c8f07fa83b8e553c97fcd24a5d590d0abd
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2023-05-14 13:47:00 -0300

    Update ObjetosClasses.py

--- ObjetosClasses.py
+++ ObjetosClasses.py
@@ -2,16 +2,22 @@
 #Objeto = objeto de fato, algo
 
 class JogadorLoteria:
-    def __init__(self):
-        self.nome = 'Pedro'
+    def __init__(self,nome):
+        self.nome = nome
         self.numeros = (12,12,11,5,73,2)
 
     def total(self):
         return sum(self.numeros)
     
-jogador_1 = JogadorLoteria()
-jogador_2 = JogadorLoteria()
+jogador_1 = JogadorLoteria('ricardo')
+jogador_2 = JogadorLoteria('richalison')
 
 print(jogador_1.nome)
 print(jogador_1.numeros)
 print(jogador_1.total())
+
+print(jogador_2.nome)
+print(jogador_2.numeros)
+print(jogador_2.total())
+
+print(jogador_1==jogador_2)
