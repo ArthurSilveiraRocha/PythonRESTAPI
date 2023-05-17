8c1783a8dfcc87dc9602990b5166dc1bb6acce07
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2023-05-17 15:37:23 -0300

    For Commit

--- Dicionario.py
+++ Dicionario.py
@@ -7,3 +7,23 @@ x = int(input('Insira um numero de 0 a 2: '))
 dicionario[2]['idade'] = '12'
 
 print(dicionario[x])
+
+d = {}
+
+for i in range (0,2):
+    f = str(input("Nome do funcionario: "))
+    ind = int(input("Indice do funcionario: "))
+    d.update({ind: f})
+
+print  ("Funcionarios cadastrados: ", d)
+
+rmv = input("deseja demitir um funcionaio? (s) para e (n) para não: \n")
+
+copia = dict(d)
+
+if rmv == "s":
+    cod = int(input("Qual o codigo do funcionario que deseja remover: \n"))
+    d.pop(cod)
+    print ("funcionario", copia.get(cod)) , "removido"
+
+print ("os funcionarios que sobraram foi: ", d)
--- ListComprehension.py
+++ ListComprehension.py
@@ -5,3 +5,10 @@ pessoas = ['Bun Da', 'M odr1 c', 'Po Br e', 'D ESEM PRE GADO', 'ch amiN e', 'do 
 pessoas_normais = [pessoa.strip().capitalize() for pessoa in pessoas]
 
 print(pessoas_normais)
+
+words = ["alpha","omega","up","down","over","under","purple","red","blue","green"]
+total = 0
+for word in words:
+    total += len(word)
+
+print(total)
