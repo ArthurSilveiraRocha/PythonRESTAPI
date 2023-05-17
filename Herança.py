d6806b8746f8681af839fa47e6f42a3f911d373b
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2023-05-17 13:10:01 -0300

    Update Herança.py

--- Herança.py
+++ Herança.py
@@ -14,8 +14,17 @@ class Admin(Funcionario):
     def __init__(self, nome, salario):
         super().__init__(nome, salario)
         
-    def atualizar_dados(self, nome):
+    def atualizar_dados(self, nome, salario):
         self.nome = nome
+        self.salario = salario
         return self.dados()
 
 vesgo = Admin('Arthur', 5000)
+
+print(yas.dados())
+print(vesgo.dados())
+
+vesgo.atualizar_dados('socorro', 0)
+
+print(vesgo.dados())
+
