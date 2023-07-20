daf2a28b2c6a8ec20c69f2bdf4addc716f3f3591
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2023-07-20 13:42:17 -0300

    commit

---
+++ Aplicacao/cria_banco.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect('banco.db')
+cursor = connection.cursor()
+
+cria_tabela = "CREATE TABLE IF NOT EXISTS hoteis\
+    (hotel_id text PRIMARY KEY,\
+    nome text,\
+    estrelas real,\
+    diaria real,\
+    cidade text)"
+
+cria_hotel = "INSERT INTO hoteis VALUES ('foxtrot', 'Foxtrot Hotel', 4.3, 345.90, 'Rio de janeiro')"
+
+cursor.execute(cria_tabela)
+cursor.execute(cria_hotel)
+
+connection.commit()
+connection.close()
