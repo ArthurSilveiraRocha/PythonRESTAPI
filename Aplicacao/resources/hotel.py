2e0dabf380820ebc1590686174626397b48ab0cb
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2023-07-25 11:43:56 -0300

    Update hotel.py

--- Aplicacao/resources/hotel.py
+++ Aplicacao/resources/hotel.py
@@ -3,53 +3,53 @@ from models.hotel import HotelModel
 
 hoteis = [
     {
-        'hotel_id':'alpha',
-        'nome':'Alpha Hotel',
-        'estrelas':4.5,
-        'diaria': 420,
-        'cidade':'Rio de Janeiro'
+        "hotel_id": "alpha",
+        "nome": "Alpha Hotel",
+        "estrelas": 4.5,
+        "diaria": 420,
+        "cidade": "Rio de Janeiro",
     },
     {
-        'hotel_id':'bravo',
-        'nome':'Bravo Hotel',
-        'estrelas':3,
-        'diaria': 120,
-        'cidade':'Balneario Camburiu'
+        "hotel_id": "bravo",
+        "nome": "Bravo Hotel",
+        "estrelas": 3,
+        "diaria": 120,
+        "cidade": "Balneario Camburiu",
     },
     {
-        'hotel_id':'charlie',
-        'nome':'Charlie Hotel',
-        'estrelas':4,
-        'diaria': 400,
-        'cidade':'Carapicuiba'
-    }
+        "hotel_id": "charlie",
+        "nome": "Charlie Hotel",
+        "estrelas": 4,
+        "diaria": 400,
+        "cidade": "Carapicuiba",
+    },
 ]
 
 
 class Hoteis(Resource):
     def get(self):
-        return{'hoteis': hoteis}
-    
+        return {"hoteis": hoteis}
+
+
 class Hotel(Resource):
-        
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
-    argumentos.add_argument('diaria')
-    argumentos.add_argument('cidade')
+    argumentos.add_argument("nome")
+    argumentos.add_argument("estrelas")
+    argumentos.add_argument("diaria")
+    argumentos.add_argument("cidade")
 
     def find_hotel(hotel_id):
         for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
+            if hotel["hotel_id"] == hotel_id:
                 return hotel
         return None
-    
+
     def get(self, hotel_id):
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
             return hotel
-        return{'message': 'Hotel not found.'}, 404 #not found
-    
+        return {"message": "Hotel not found."}, 404  # not found
+
     def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
         hotel_objeto = HotelModel(hotel_id, **dados)
@@ -67,11 +67,11 @@ class Hotel(Resource):
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
-            return novo_hotel, 200 # OK
+            return novo_hotel, 200  # OK
         hoteis.append(novo_hotel)
-        return novo_hotel, 201 #criado
+        return novo_hotel, 201  # criado
 
     def delete(self, hotel_id):
         global hoteis
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-        return {'message':'Hotel deleted'}
+        hoteis = [hotel for hotel in hoteis if hotel["hotel_id"] != hotel_id]
+        return {"message": "Hotel deleted"}
